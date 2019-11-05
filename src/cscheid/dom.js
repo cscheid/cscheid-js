1ccfd36b9765c53da2d6d0269ff7ca98eb5f0f13
Carlos Scheidegger <cscheid@cs.arizona.edu>
2019-11-05 09:24:10 +0100

    documentation, animate

--- src/cscheid/dom.js
+++ src/cscheid/dom.js
@@ -1,7 +1,19 @@
 import * as cscheid from "../cscheid.js";
 
-// https://stackoverflow.com/questions/618097/how-do-you-easily-horizontally-center-a-div-using-css
+/*
+ * dom.js: interact with the Document Object Model (and some web browser stuff)
+ */
+
+/** 
+ * creates elements (by default divs) that are horizontally centered
+ * on the webpage
+ *
+ * @param {sel} input d3 selection with parents
+ * @param {el}  input element name, by default "div"
+ * @returns {d3 selection} selection containing created elements
+ */
 export function makeCenteredElement(sel, el) {
+  // https://stackoverflow.com/questions/618097/how-do-you-easily-horizontally-center-a-div-using-css
   el = el || "div";
   return sel.append("div")
     .style("text-align", "center")
@@ -9,8 +21,17 @@ export function makeCenteredElement(sel, el) {
     .style("display", "inline-block");
 };
 
-// https://www.html5rocks.com/en/tutorials/canvas/hidpi/
+/**
+ * Creates canvas drawing context and sets up High-DPI rendering,
+ * keeping the rendering coordinate systems consistent with the
+ * low-DPI case
+ * 
+ * @param {canvas} input canvas DOM element
+ * @returns {Canvas drawing context} the newly created canvas drawing context
+ */
+// FIXME: should `canvas` be a d3 selection?
 export function setupCanvas(canvas) {
+  // https://www.html5rocks.com/en/tutorials/canvas/hidpi/
   var dpr = window.devicePixelRatio || 1;
   // Get the size of the canvas in CSS pixels.
   var rect = canvas.getBoundingClientRect();
@@ -33,6 +54,14 @@ export function setupCanvas(canvas) {
 };
 
 // this is slow!
+/**
+ * Converts position from any CSS units to pixel units. This is useful, for
+ * example, to determine the vertical center of a window by calling it with '0.5vh',
+ * and so on.
+ *
+ * @param {size} input size description, as a string
+ * @returns {Number} size in CSS pixels
+ */
 export function convertToPixelUnits(size) {
   var d = d3.select("body").append("div").style("position", "absolute").style("left", size);
   var v1 = d.node().getBoundingClientRect().x;
@@ -40,4 +69,25 @@ export function convertToPixelUnits(size) {
   var v2 = d.node().getBoundingClientRect().x;
   d.remove();
   return v1 - v2;
+}
+
+/** 
+ * Sets up an animation callback loop with requestAnimationFrame.
+ * 
+ * @param {fun} input animation callback to be called at every rendering tick.
+ * @returns {stop callback} a callback that will stop the animation.
+ */
+export function animate(fun) {
+  let stop = false;
+  function tick() {
+    fun();
+    if (!stop) {
+      window.requestAnimationFrame(tick);
+    }
+  }
+  function stopAnimation() {
+    stop = true;
+  }
+  window.requestAnimationFrame(tick);
+  return stopAnimation;
 }
