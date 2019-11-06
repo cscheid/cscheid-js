d172740e82ee12070997586f308dc6fbc1058500
Carlos Scheidegger <cscheid@cs.arizona.edu>
2019-11-06 12:02:41 +0100

    comments

--- src/cscheid/css.js
+++ src/cscheid/css.js
@@ -1,10 +1,18 @@
-import * as cscheid from "../cscheid.js";
+import centeredTextRotate from "./svg.js";
 
+/**
+ * Centers text horizontally
+ * @param {sel} input d3 selection with text
+ */
 export function centerHorizontalText(sel) {
   sel.attr("text-anchor", "middle");
 }
 
+/**
+ * Centers text, then rotates it by -90 degrees around its center
+ * @param {sel} input d3 selection with text
+ */
 export function centerVerticalText(sel) {
   sel.attr("text-anchor", "middle")
-    .attr("transform", cscheid.svg.centeredTextRotate(-90));
+    .attr("transform", centeredTextRotate(-90));
 }
